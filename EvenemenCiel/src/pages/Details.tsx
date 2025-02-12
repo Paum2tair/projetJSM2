ea21ee40e35393081606065808c5beaa4fd505e8
antoinePROVAIN <antoineprovain@gmail.com>
2025-02-12 15:30:13 +0100

    lien entre page

---
+++ EvenemenCiel/src/pages/Details.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Details = () => {
+
+  return (
+    
+    <div className='details'>
+      <h1>Details</h1>
+    </div>
+
+  );
+};
+
+export default Details;
---
+++ EvenemenCiel/src/pages/Panier.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Panier = () => {
+
+  return (
+    
+    <div className='panier'>
+      <h1>Panier</h1>
+    </div>
+
+  );
+};
+
+export default Panier;
