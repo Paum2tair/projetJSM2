00c1065b65a836ec3f80d5911e3155ec9e80d0e1
antoinePROVAIN <antoineprovain@gmail.com>
2025-02-13 11:27:38 +0100

    form

--- EvenemenCiel/src/components/Form.tsx
+++ EvenemenCiel/src/components/Form.tsx
@@ -2,17 +2,19 @@ import "../assets/css/form.css"
 
 const Form = () => {
     return (
-      <div className="form">
-        <h3>Réservation de l'événement</h3>
-  
-        <form action="/submit" method="POST">
-          <input type="text" id="name" name="name" placeholder="Name" required />
-          <input type="email" id="email" name="email" placeholder="Email" required />
-          <input type="number" id="places" name="places" placeholder="1" min="1" required />
-  
-          <button type="submit">Valider</button>
-        </form>
-      </div>
+        <>
+        
+          <h3>Réservation de l'événement</h3>
+          <div className="forme">
+            <form action="/submit" method="POST" className="submit_f">
+              <input type="text" id="name" name="name" placeholder="Name" required />
+              <input type="email" id="email" name="email" placeholder="Email" required />
+              <input type="number" id="places" name="places" placeholder="1" min="1" required />
+      
+              <button type="submit">Valider</button>
+            </form>
+        </div>
+      </>
     );
   };
   
