d0d7f20b3c5879d19cf1d02a5247232458097140
Emie18 <lalala.emiliat@gmail.com>
2025-02-13 14:53:29 +0100

    💄 css ++💄

--- EvenemenCiel/src/components/Footer.tsx
+++ EvenemenCiel/src/components/Footer.tsx
@@ -4,6 +4,7 @@ const Footer = () => {
   return (
     
     <div className='footer'>
+      <p>Emilie Le Rouzic + Antoine * (Provain + Noel)</p>
         {/* <img id="down_cloud" src="/footer2.png" alt="NuagesEnBas"/> */}
     </div>
 
