f40f183f64ed070bb047a0ea7f12fbc01acfbbb3
antoinePROVAIN <antoineprovain@gmail.com>
2025-03-11 16:43:11 +0100

    😊 ajout du darkmode v1

--- EvenemenCiel/src/components/Header.tsx
+++ EvenemenCiel/src/components/Header.tsx
@@ -1,21 +1,42 @@
+import { useEffect, useState } from "react";
 import {useNavigate } from "react-router-dom";
 
 const Header = () => {
 
   const navigate = useNavigate();
+  const [isDark, setIsDark] = useState(false);
   
   const allerVersHome = () => {
     navigate('/');
   };
+  
+  const themeChange = () => {
+    const newTheme = isDark ? "light" : "dark";
+    setIsDark(!isDark);
+    document.body.classList.toggle("dark-theme");
+    localStorage.setItem("theme", newTheme);
+    window.dispatchEvent(new Event("themeChanged"));
+  };
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+      setIsDark(true);
+      document.body.classList.add("dark-theme");
+    }
+  }, []);
 
     return (
 
       <div className='header'>
-          <div className='circle' onClick={allerVersHome} title="Accueil">
+          <div className={`circle ${isDark ? "circle_color_dark" : "circle_color_light"}`} onClick={allerVersHome} title="Accueil">
             <h1>EvenemenCiel</h1>
           </div>
           <div className='panier-container'>
             <a href='/panier'><img src="/panier.png"></img></a>
+          </div>
+          <div className='them-container'>
+            <img src={isDark ? "/sun.png" : "/moon.png"} onClick={themeChange}></img>
           </div>
       </div>
 
