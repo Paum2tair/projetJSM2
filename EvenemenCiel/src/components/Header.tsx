a1f54156cb4017d2c7ed48b65dccb7eb3eac8d99
Emie18 <lalala.emiliat@gmail.com>
2025-03-11 08:22:48 +0100

    ✨ retour click ✨

--- EvenemenCiel/src/components/Header.tsx
+++ EvenemenCiel/src/components/Header.tsx
@@ -1,9 +1,17 @@
+import {useNavigate } from "react-router-dom";
+
 const Header = () => {
+
+  const navigate = useNavigate();
+  
+  const allerVersHome = () => {
+    navigate('/');
+  };
 
     return (
 
       <div className='header'>
-          <div className='circle'>
+          <div className='circle' onClick={allerVersHome} title="Accueil">
             <h1>EvenemenCiel</h1>
           </div>
           <div className='panier-container'>
