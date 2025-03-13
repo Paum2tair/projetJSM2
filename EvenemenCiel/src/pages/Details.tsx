6b809481e52c4391ba25949e6906deb5de9b0501
Emie18 <lalala.emiliat@gmail.com>
2025-03-13 16:12:05 +0100

    a

--- EvenemenCiel/src/pages/Details.tsx
+++ EvenemenCiel/src/pages/Details.tsx
@@ -7,10 +7,11 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { Event } from "../scripts/Event";
 import Etoiles from "../components/Etoiles";
+import { getAllEvents } from "../scripts/GetAll";
 interface DetailsProps {
-    events: Event[];
-  }
-const Details: React.FC<DetailsProps> = ({events}) => {
+  events: Event[];
+}
+const Details: React.FC<DetailsProps> = ({ events }) => {
   const { id } = useParams<{ id: string }>(); // Récupérer l'id de l'URL
   const [event, setEvent] = useState<Event | null>(null);
   const [error, setError] = useState<string | null>(null);
@@ -31,14 +32,20 @@ const Details: React.FC<DetailsProps> = ({events}) => {
   }, [showForm, localStorageShowFormKey]);
 
   useEffect(() => {
-    console.log("id:",id);
-    const foundEvent = events.find((event) => event.id.toString() === id);
-        console.log('find',foundEvent);
-        if (!foundEvent) {
-          setError("Événement introuvable");
-        } else {
-          setEvent(foundEvent);
-        }
+    console.log("id:", id);
+    let foundEvent = events.find((event) => event.id.toString() === id);
+    console.log('find', foundEvent);
+    if (!foundEvent) {
+      getAllEvents()
+        .then((data: Event[]) => {
+          localStorage.setItem("data", JSON.stringify(data));
+          const event2 = data;
+          foundEvent = event2.find((event) => event.id.toString() === id);
+        })
+        .catch(error => setError(error.message));
+    } else {
+      setEvent(foundEvent);
+    }
 
   }, [id]); // Recharger si l'ID change
 
@@ -71,17 +78,17 @@ const Details: React.FC<DetailsProps> = ({events}) => {
               <p><strong>Lieu :</strong> {event.location} </p>
               <p><strong>Organisateur :</strong> {event.organizer}</p>
               <p><strong>Prix :</strong> {event.price.toFixed(2)} <img src="/euro.png" alt="Euro" /></p>
-              
-              <button 
-                className="reservation-button" 
+
+              <button
+                className="reservation-button"
                 onClick={toggleForm}
               >
                 {showForm ? "Masquer le formulaire" : "Réserver"}
               </button>
             </div>
           </div>
-          
-          <div 
+
+          <div
             className={`form-container ${showForm ? 'open' : 'closed'}`}
           >
             {showForm && <Form eventId={event.id} max_places={event.max_attendees} evente={event} />}
